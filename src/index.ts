f6613c551f4c37c51245965a8900c86a55449b1b
Alberto Méndez Hernández <albertomendezhdz@gmail.com>
2019-12-10 12:24:21 +0100

    Added Spinner export member

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import Button from './components/Button'
 import Input from './components/Input'
 import Provider from './components/Provider'
+import Spinner from './components/Spinner'
 import defaultTheme from './default-theme'
 export * from './models'
 
@@ -8,5 +9,6 @@ export {
   defaultTheme,
   Button,
   Input,
-  Provider
+  Provider,
+  Spinner
 }
