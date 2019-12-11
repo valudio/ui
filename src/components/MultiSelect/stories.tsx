237c8f5ebe8642ce80c23c8eba99a84d0dd05e98
Marc Fuentes Montenegro <marcf.montenegro@gmail.com>
2019-12-11 21:31:46 +0100

    newline tslint fix

--- src/components/MultiSelect/stories.tsx
+++ src/components/MultiSelect/stories.tsx
@@ -6,4 +6,7 @@ import Provider from '../Provider'
 
 storiesOf('MultiSelect', module)
   .addDecorator(x => <Provider>{ x() }</Provider>)
-  .add('default', () => <MultiSelect options={ optionsMock } labelProp="value" onChange={ alert } />)
+  .add('default', () => <MultiSelect placeholder="Select..." options={ optionsMock } labelProp="value" onChange={ console.log } />)
+  .add('disabled', () => <MultiSelect placeholder="Select..." options={ [] } labelProp="value" onChange={ console.log } />)
+  .add('invalid', () => <MultiSelect placeholder="Select..." options={ optionsMock } labelProp="value" onChange={ console.log } isInvalid={ true }/>)
+  .add('hidden', () => <MultiSelect placeholder="Select..." options={ optionsMock } labelProp="value" onChange={ console.log } isHidden={ true } />)
