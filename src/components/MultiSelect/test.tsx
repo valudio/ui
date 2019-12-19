61318dfdb9bd5d956d08fba4fcd2b2e6731b25ca
Marc Fuentes Montenegro <marcf.montenegro@gmail.com>
2019-12-19 11:37:13 +0100

    update tests

--- src/components/MultiSelect/test.tsx
+++ src/components/MultiSelect/test.tsx
@@ -1,37 +1,16 @@
 import React from 'react'
-import { act, create, ReactTestInstance, ReactTestRenderer } from 'react-test-renderer'
-import defaultTheme from '../../default-theme'
+import renderer from 'react-test-renderer'
 import { optionsMock } from '../../models'
-import MultiSelect from '../MultiSelect'
 import Provider from '../Provider'
-import Item from './Item'
+import Multiselect from './'
 
-let component: ReactTestRenderer
-let instance: ReactTestInstance
+// tslint:disable: no-console
 
-beforeEach(() => {
-  component = create(
-    <Provider theme={ defaultTheme }>
-      <MultiSelect onChange={ alert } labelProp="label" options={ optionsMock } />
+test('Multiselect', () => {
+  const component = renderer.create(
+    <Provider>
+      <Multiselect placeholder="Select..." options={ optionsMock } labelProp="value" onChange={ console.log } />
     </Provider>
   )
-  instance = component.root
-})
-
-describe('MultiSelect', () => {
-  it('should be correctly rendered', () => {
-    expect(component).toBeTruthy()
-  })
-
-  it('options are hidden by default', () => {
-    expect(instance.findAllByType(Item)).toEqual([])
-  })
-
-  it('options are shown after click on selected label', () => {
-    const selected = instance.findByType('div')
-    act(() => {
-      selected.props.onClick()
-    })
-    expect(instance.findAllByType(Item).length).toEqual(7)
-  })
+  expect(component).toBeTruthy()
 })
