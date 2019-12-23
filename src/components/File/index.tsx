450cd9164e5eac9d36950e0b246ab7369e598871
Marc Fuentes Montenegro <marcf.montenegro@gmail.com>
2019-12-23 11:39:18 +0100

    click on file implemented

--- src/components/File/index.tsx
+++ src/components/File/index.tsx
@@ -6,13 +6,14 @@ import Styled from './styles'
 interface IProps extends IBaseProps {
   type: FileType
   name: string
+  onClick?: () => void
 }
 
-const File: React.FC<IProps> = ({ name, isHidden, className, type, style }) => {
+const File: React.FC<IProps> = ({ name, isHidden, className, type, style, onClick }) => {
   if (isHidden) return null
 
   return (
-    <Styled className={ className } style={ style }>
+    <Styled className={ className } style={ style } onClick={ onClick }>
       <Icon className="icon" icon={ type } />
       <span className="name">{ name }</span>
     </Styled>
