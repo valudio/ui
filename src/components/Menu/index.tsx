dd4f317522aea5d9092f4a372d63fc39831a8078
Marc Fuentes Montenegro <marcf.montenegro@gmail.com>
2019-12-30 10:26:32 +0100

    menu is now fixed

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -14,26 +14,28 @@ const Menu: React.FC<IProps> = ({ children, isHidden, className, style, language
   if (isHidden) return null
 
   const [ isExpanded, setIsExpanded ] = useState(false)
-  const classNames = `${ className || '' } ${ isExpanded ? 'expanded' : '' }`
+  const classNames = `wrapper ${ isExpanded ? 'expanded' : '' }`
   const items = Children.map(children, x => isValidElement(x) && cloneElement(x, { className: 'item', isExpanded }))
 
   return (
-    <Styled className={ classNames } style={ style }>
-      <button className="logo" onClick={ setIsExpanded.bind(undefined, !isExpanded) } />
-      <section className="items">{ items }</section>
-      <LanguageItem
-        isHidden={ !languages || !languages.length }
-        isExpanded={ isExpanded }
-        languages={ languages || [] }
-        onLanguageClick={ onLanguageClick }
-      />
-      <MenuItem
-        isExpanded={ isExpanded }
-        isHidden={ !username }
-        label={ username || '' }
-        className="username"
-        icon={ <div className="badge" /> as ReactNode }
-      />
+    <Styled className={ className || '' } style={ style }>
+      <div className={ classNames } >
+        <button className="logo" onClick={ setIsExpanded.bind(undefined, !isExpanded) } />
+        <section className="items">{ items }</section>
+        <LanguageItem
+          isHidden={ !languages || !languages.length }
+          isExpanded={ isExpanded }
+          languages={ languages || [] }
+          onLanguageClick={ onLanguageClick }
+        />
+        <MenuItem
+          isExpanded={ isExpanded }
+          isHidden={ !username }
+          label={ username || '' }
+          className="username"
+          icon={ <div className="badge" /> as ReactNode }
+        />
+      </div>
     </Styled>
   )
 }
