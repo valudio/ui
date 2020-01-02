365fa567e7c59d5ad81993921397ef71b366e952
Alberto Méndez Hernández <albertomendezhdz@gmail.com>
2020-01-02 20:46:31 +0100

    Added missing file icons, or logic for generic type instead of add more icons

--- src/components/File/index.tsx
+++ src/components/File/index.tsx
@@ -12,12 +12,22 @@ interface IProps extends IBaseProps {
 
 const File: React.FC<IProps> = ({ name, isHidden, className, type, style, onClick, isDisabled }) => {
   if (isHidden) return null
-
   const classNames = `${ className || '' } ${ isDisabled ? 'disabled' : '' }`
+  let icon
+
+  if (type === 'sap' || type === 'idoc') {
+    icon = 'sap'
+  } else if (type === 'excel' || type === 'xls' || type === 'csv') {
+    icon = 'excel'
+  } else if (type === 'jpg' || type === 'jpeg' || type === 'png' || type === 'tif' || type === 'tiff') {
+    icon = 'img'
+  } else {
+    icon = type
+  }
 
   return (
     <Styled className={ classNames } style={ style } onClick={ !isDisabled && onClick }>
-      <Icon className="icon" icon={ type } />
+      <Icon className="icon" icon={ icon } />
       <span className="name">{ name }</span>
     </Styled>
   )
