360f6eb1c4deb48b14aef3d3ae4d707cbeca642c
Marc Fuentes Montenegro <marcf.montenegro@gmail.com>
2020-02-05 22:24:35 +0100

    modal container flex and fix select open event

--- src/helpers/dom.ts
+++ src/helpers/dom.ts
@@ -1,6 +1,8 @@
 export const isChildNode = (parent, child): boolean => {
+  if (parent === child) return true
   let node = child.parentNode
-  while (node != null) {
+
+  while (node !== null) {
     if (node === parent) return true
     node = node.parentNode
   }
