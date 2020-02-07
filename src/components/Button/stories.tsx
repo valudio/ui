b3ee2a275e47a216c8008b8ce5afdaf3c17e9d05
Marc Fuentes Montenegro <marcf.montenegro@gmail.com>
2020-02-07 10:28:57 +0100

    Circular button

--- src/components/Button/stories.tsx
+++ src/components/Button/stories.tsx
@@ -1,6 +1,7 @@
 import { storiesOf } from '@storybook/react'
 import React from 'react'
 import { decorator } from '../../helpers/storybook'
+import Icon from '../Icon'
 import Button from './'
 
 storiesOf('Button', module)
@@ -10,3 +11,5 @@ storiesOf('Button', module)
   .add('secondary', () => <Button isSecondary>Click me!</Button>)
   .add('secondary disabled', () => <Button isSecondary isDisabled>Click me!</Button>)
   .add('hidden', () => <Button isHidden>Click me!</Button>)
+  .add('circular', () => <Button isCircular><Icon icon="edit" /></Button>)
+  .add('circular secondary', () => <Button isCircular isSecondary><Icon icon="edit" /></Button>)
