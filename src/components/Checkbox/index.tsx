73a1e538705c23f8e049691a0355e81c6b933847
Marc Fuentes Montenegro <marcf.montenegro@gmail.com>
2020-02-11 10:26:54 +0100

    lint fix

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { IInputProps } from '../../models'
 import Icon from '../Icon'
 import Styled from './styles'
