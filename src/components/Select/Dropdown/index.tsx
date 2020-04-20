3ca1ff3887a71cdebd6c243c77d07eb4cf80efaa
Alberto Méndez Hernández <albertomendezhdz@gmail.com>
2020-04-20 14:22:52 +0200

    🐛 Fix Select dropdown position on open

--- src/components/Select/Dropdown/index.tsx
+++ src/components/Select/Dropdown/index.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { RefObject } from 'react'
 import { IBaseProps } from '../../../models'
 import Styled from './styles'
 
-const Dropdown: React.FC<IBaseProps> = ({ isHidden, children, className }) => {
-  if (isHidden) return null
+interface IProps extends IBaseProps {
+  parentRef: RefObject<HTMLDivElement>
+}
 
-  return <Styled className={ className || '' }>{ children }</Styled>
+const Dropdown: React.FC<IProps> = ({ isHidden, children, className, parentRef }) => {
+  if (isHidden) return null
+  const selectContainer = parentRef.current.getBoundingClientRect()
+  const dropdownPosition = {
+    top: selectContainer.top + 48,
+    left: selectContainer.left
+  }
+
+  return <Styled className={ className || '' } style={ dropdownPosition } >{ children }</Styled>
 }
 
 export default Dropdown
