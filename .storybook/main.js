9e4a625b28c14ea8dc98d98fc6c77d52988b17b3
Alberto Méndez Hernández <albertomendezhdz@gmail.com>
2021-09-21 23:57:23 +0200

    Fix storybook generation

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,6 @@
 const path = require('path');
 
 module.exports = {
-  typescript: {
-    check: false,
-    checkOptions: {},
-    reactDocgen: 'react-docgen-typescript',
-    reactDocgenTypescriptOptions: {
-      shouldExtractLiteralValuesFromEnum: true,
-      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
-    },
-  },
   stories: ['../src/**/stories.tsx'],
   addons: [
     {
@@ -22,5 +13,14 @@ module.exports = {
       },
     },
     'storybook-addon-jsx'
-  ]
+  ],
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+    },
+  }
 };
