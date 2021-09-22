adf840491044c86bc414ecb537cd8a53b68d0d7b
Alberto Mendez Hernandez <albertomendezhdz@gmail.com>
2021-09-22 15:45:51 +0200

    🗑️  Code cleanup

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,17 +1,10 @@
 import { createGlobalStyle } from 'styled-components'
 import iconsTtf from '../assets/fonts/icons.ttf'
-import iconsWoff from '../assets/fonts/icons.woff'
-import iconsEot from '../assets/fonts/icons.eot'
-import iconsSvg from '../assets/fonts/icons.svg'
 
 export default createGlobalStyle`
   @font-face {
     font-family: 'Icons';
-    src: url(${ iconsEot });
-    src: url(${ iconsEot }) format("embedded-opentype"),
-      url(${ iconsWoff }) format("woff"),
-      url(${ iconsTtf }) format("truetype"),
-      url(${ iconsSvg }) format("svg");
+    src: url(${ iconsTtf }) format("truetype");
     font-weight: normal;
     font-style: normal;
   }
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,8 +1,1 @@
 declare module '*.ttf';
-declare module '*.eot';
-declare module '*.woff';
-declare module '*.woff2';
-declare module '*.svg' {
-  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
-  export default content;
-}
